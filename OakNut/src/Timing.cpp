344e984695f32589c86fdaac8d3d7b488804bf68
Daivuk <daivuk@gmail.com>
2015-10-03 09:51:36 -0400

    FPS counter

---
+++ OakNut/src/Timing.cpp
@@ -0,0 +1,35 @@
+#include "Timing.h"
+
+void onut::Timing::onCreate()
+{
+    m_lastTime = std::chrono::high_resolution_clock::now();
+}
+
+void onut::Timing::onUpdate()
+{
+    auto now = std::chrono::high_resolution_clock::now();
+    m_deltaTime = now - m_lastTime;
+    m_lastTime = now;
+
+    m_fDeltaTime = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(m_deltaTime).count()) / 1000.0f;
+}
+
+void onut::Timing::onDraw()
+{
+    auto now = std::chrono::high_resolution_clock::now();
+    if (now - m_fpsTime >= std::chrono::seconds(1))
+    {
+        m_fpsTime = now;
+        m_fps = m_fpsCounter;
+        m_fpsCounter = 0;
+    }
+    else
+    {
+        ++m_fpsCounter;
+    }
+}
+
+float onut::Timing::getDeltaTime() const
+{
+    return m_fDeltaTime;
+}
--- OakNut/src/Timing.h
+++ OakNut/src/Timing.h
@@ -1,11 +1,26 @@
 #pragma once
 #include "Component.h"
 
+#include <chrono>
+
 namespace onut
 {
     class Timing : public Component
     {
     public:
+        void onCreate() override;
+        void onUpdate() override;
+        void onDraw() override;
 
+        float getDeltaTime() const;
+        int getFPS() const;
+
+    private:
+        std::chrono::high_resolution_clock::time_point m_lastTime;
+        std::chrono::high_resolution_clock::duration m_deltaTime;
+        std::chrono::high_resolution_clock::time_point m_fpsTime;
+        int m_fpsCounter = 0;
+        float m_fDeltaTime = 0.f;
+        int m_fps = 0;
     };
 }
